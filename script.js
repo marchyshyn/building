677b72757523ec4699cc49f37b833afc0b6fe78c
Marchyshin Andriy <amintux@gmail.com>
2015-11-17 00:35:05 +0200

    v0.0.1

--- script.js
+++ script.js
@@ -4,16 +4,23 @@ var canvas = $('#workSpace')[0],
 	canvasHeight = $('#workSpace').height(),
 	randomBlock,
 	block,
+	allBlock = [],
+	empty = [],
 	direction,
+	speed,
+	localCount = 0,
+	cellSize,
 	randomBlock;
 
 
 
 	function init() {
 		direction = 'down';
+		speed = 100;
+		cellSize = 20;
 		createBlock();
-		paint();
-		//loop = setInterval(paint, 1000);
+		paintDown();
+		loop = setInterval(paintDown, speed);
 	}
 
 	function createBlock() {
@@ -22,29 +29,61 @@ var canvas = $('#workSpace')[0],
 			[{x:7, y:0},{x:8, y:0},{x:9, y:0},{x:10, y:0}],
 			[{x:7, y:0},{x:8, y:0},{x:9, y:0},{x:7, y:1}],
 			[{x:7, y:0},{x:8, y:0},{x:8, y:1},{x:9, y:1}],
-			[{x:7, y:1},{x:8, y:1},{x:8, y:0},{x:9, y:0}],
+			[{x:9, y:0},{x:8, y:1},{x:8, y:0},{x:7, y:1}],
 			[{x:9, y:0},{x:8, y:1},{x:9, y:1},{x:10, y:1}],
 			[{x:8, y:0},{x:9, y:0},{x:8, y:1},{x:9, y:1}]
 			);
-		randomBlock = Math.floor(Math.random() * (1 - 0)) + 0;
+		randomBlock = Math.floor(Math.random() * (6 - 0)) + 0;
 	}
 
-	function paint() {
-		ctx.strokeStyle = 'black';
+	function setLoop(speed){
+		clearInterval(loop);
+		loop = setInterval(paintDown, speed); 
+	}
+
+	function newBlock(){
+		if(block[randomBlock][3].y == 27){
+			allBlock.push(block[randomBlock]);
+			//var qq = block[randomBlock].x.join();
+			//console.log(qq);
+			clearInterval(loop);
+			init();
+		}
+		if(allBlock.length > 0){
+			for(var i = 0; i < allBlock.length; i++){;
+			for(var k = 0; k < allBlock[i].length; k++){
+				var c = allBlock[i][k];
+				paintCanvas(c.x, c.y);
+			}
+		}
+		}
+	}
+
+	function paintDown() {
+		ctx.fillStyle = 'black';
+		ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+		ctx.strokeStyle = 'red';
 		ctx.strokeRect(0, 0, canvasWidth, canvasHeight);
 		var count = block[randomBlock].length;
 
 		for (var i = 0; i < count; i++) {
 			var nx = block[randomBlock][i].x;
 			var ny = block[randomBlock][i].y;
+			if(direction == 'down') {	
 				ny++;
-				var tail = block[randomBlock][i];
-				var del = block[randomBlock].shift();
-			tail.x = nx;
+			}
+			else if(direction == 'left') {
+				nx--;
+			}
+			else if(direction == 'right') {
+				nx++;
+			}
+			var tail = block[randomBlock][i];
 			tail.y = ny;
-			block[randomBlock].unshift(tail);	
+			tail.x = nx;
 		};
-
+		direction = 'down';
+		newBlock();
 		for(var i = 0; i < block[randomBlock].length; i++){
 			var c = block[randomBlock][i];
 			paintCanvas(c.x, c.y);
@@ -53,10 +92,18 @@ var canvas = $('#workSpace')[0],
 
 	function paintCanvas(x, y) {
 		ctx.fillStyle = 'red';
-		ctx.fillRect(x*20, y*20, 20, 20);
+		ctx.fillRect(x*cellSize, y*cellSize, cellSize, cellSize);
+		ctx.strokeStyle = 'black';
+		ctx.strokeRect(x*cellSize, y*cellSize, cellSize, cellSize);
 	}
+$(document).keydown(function(e){
+		var key = e.which;
+		if(key == '37') direction = 'left';
+		if(key == '38') direction = 'up';
+		if(key == '39') direction = 'right';
+		if(key == '40') direction = 'down';
+	})
 
-
-$(window).ready(function() {
+$(window).load(function() {
 	init();
 })
